0f131956df16f9c057038036a5edca75f0a20f37
Piyush023 <piyush.khurana@rario.com>
2024-08-14 19:29:11 +0530

    Arrays Intro and Image Change Logic Upadted acc to Arrays.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//     IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    var image1Index = 1
+    var image2Index = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named: "DiceSix")
+//        diceImageView1.alpha = 0.4
+        diceImageView2.image = UIImage(named: "DiceTwo")
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+//        Arrays - Intro - 14/08 - 
+        let diceImageView1Array = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+        diceImageView1.image = diceImageView1Array[image1Index]
+        image1Index = image1Index + 1
+        
+        diceImageView2.image = diceImageView1Array[image2Index]
+        image2Index = image2Index - 1
+    }
+    
 }
 
